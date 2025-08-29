17887fe0b3270d6987d46ac6f7f1621e2754a215
Alex Boyce-Draeger <boycedal@dukes.jmu.edu>
2025-08-29 16:31:50 -0400

    Class

--- p1-lexer/src/p1-lexer.c
+++ p1-lexer/src/p1-lexer.c
@@ -4,10 +4,46 @@
  */
 #include "p1-lexer.h"
 
-TokenQueue* lex (const char* text)
+TokenQueue *
+lex (const char *text)
 {
-    TokenQueue* tokens = TokenQueue_new();
+  TokenQueue *tokens = TokenQueue_new ();
 
-    return tokens;
+  /* compile regular expressions */
+  Regex *whitespace = Regex_new ("^[ \n]");
+  Regex *letter = Regex_new ("^[a-z]");
+
+  /* read and handle input */
+  // Read through decaf and understand program
+  char match[MAX_TOKEN_LEN];
+  while (*text != '\0')
+    {
+
+      /* match regular expressions */
+      if (Regex_match (whitespace, text, match))
+        {
+          /* ignore whitespace */
+        }
+      else if (Regex_match (letter, text, match))
+        {
+          /* TODO: implement line count and replace placeholder (-1) */
+          TokenQueue_add (tokens, Token_new (ID, match, -1));
+        }
+      else
+        {
+          Error_throw_printf ("Invalid token!\n");
+        }
+
+      /* skip matched text to look for next token */
+      text += strlen (match);
+    }
+
+  /* clean up */
+  Regex_free (whitespace);
+  Regex_free (letter);
+
+  return tokens;
 }
 
+return tokens;
+}
