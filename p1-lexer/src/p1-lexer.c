f076ab54f43c40eb94c6d7499baa24ef86d8e02a
pure_jerome <donfacjc@dukes.jmu.edu>
2025-08-29 19:09:11 -0400

    all symbols added, strings and hexcode added

--- p1-lexer/src/p1-lexer.c
+++ p1-lexer/src/p1-lexer.c
@@ -8,12 +8,14 @@ TokenQueue *
 lex (const char *text)
 {
   TokenQueue *tokens = TokenQueue_new ();
-
+//"^(==|<=|>=|&&|\\|\\||[()\\+\\*{}\\[\\],;=\\-\\/<>!%])"
   /* compile regular expressions */
   Regex *whitespace = Regex_new ("^[ \n]");
-  Regex *symbol = Regex_new("^[()\\+\\*]");
+  Regex *symbol = Regex_new("^(==|<=|>=|!=|&&|\\|\\||[][(){};=,+*\\-\\/%<>!])");
   Regex *decimal_int = Regex_new("^(0|[1-9][0-9]*)");
-  Regex *identifier = Regex_new("^([a-z A-Z][a-z A-Z 0-9]*)");
+  Regex *identifier = Regex_new("^([a-z A-Z][a-zA-Z0-9]*)");
+  Regex *string = Regex_new("^\"([^\"]*)\"");
+  Regex *hex_literal = Regex_new("^(0x[0-9a-fA-F]+)");
 
   /* read and handle input */
   /* Read through decaf and understand program*/
@@ -39,10 +41,15 @@ lex (const char *text)
           /* TODO: implement line count and replace placeholder (-1) */
           TokenQueue_add (tokens, Token_new (SYM, match, line_count));
         }
-      else if (Regex_match (decimal_int, text, match))
+      else if (Regex_match(hex_literal, text, match) || Regex_match (decimal_int, text, match))
+        {
+          bool hex_match = Regex_match(hex_literal, text, match);
+          TokenQueue_add (tokens, Token_new (hex_match ? HEXLIT : DECLIT, match, line_count));
+        }
+      else if (Regex_match (string, text, match))
         {
           /* TODO: implement line count and replace placeholder (-1) */
-          TokenQueue_add (tokens, Token_new (DECLIT, match, line_count));
+          TokenQueue_add (tokens, Token_new (STRLIT, match, line_count));
         }
       else
         {
