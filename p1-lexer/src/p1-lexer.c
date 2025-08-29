5958936543b4542d023ce2b54cc618fbaa3c6567
pure_jerome <donfacjc@dukes.jmu.edu>
2025-08-28 21:31:22 -0400

    p1 added

---
+++ p1-lexer/src/p1-lexer.c
@@ -0,0 +1,13 @@
+/**
+ * @file p1-lexer.c
+ * @brief Compiler phase 1: lexer
+ */
+#include "p1-lexer.h"
+
+TokenQueue* lex (const char* text)
+{
+    TokenQueue* tokens = TokenQueue_new();
+
+    return tokens;
+}
+
