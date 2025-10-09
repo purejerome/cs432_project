da2ce72bd54e352e3b6a954d730f0f81904d903c
pure_jerome <donfacjc@dukes.jmu.edu>
2025-10-08 21:51:37 -0400

    decent yet shaky start into p3

--- p3-analysis/src/p3-analysis.c
+++ p3-analysis/src/p3-analysis.c
@@ -15,6 +15,7 @@ typedef struct AnalysisData
     ErrorList* errors;
 
     /* BOILERPLATE: TODO: add any new desired state information (and clean it up in AnalysisData_free) */
+    DecafType current_function_type;
 
 } AnalysisData;
 
@@ -28,6 +29,7 @@ AnalysisData* AnalysisData_new (void)
     AnalysisData* data = (AnalysisData*)calloc(1, sizeof(AnalysisData));
     CHECK_MALLOC_PTR(data);
     data->errors = ErrorList_new();
+    data->current_function_type = VOID;
     return data;
 }
 
@@ -40,7 +42,6 @@ void AnalysisData_free (AnalysisData* data)
 {
     /* free everything in data that is allocated on the heap except the error
      * list; it needs to be returned after the analysis is complete */
-
     /* free "data" itself */
     free(data);
 }
@@ -89,6 +90,120 @@ Symbol* lookup_symbol_with_reporting(NodeVisitor* visitor, ASTNode* node, const 
 void AnalysisVisitor_infer_literal (NodeVisitor* visitor, ASTNode* node)
 {
     SET_INFERRED_TYPE(node->literal.type);
+}
+
+void AnalysisVisitor_screen_vardecl (NodeVisitor* visitor, ASTNode* node)
+{
+    DecafType var_type = node->vardecl.type;
+    if (var_type == VOID) {
+        ErrorList_printf(ERROR_LIST, "Type error on line %d: variable declared with void type", node->source_line);
+    }
+    if (node->vardecl.is_array && node->vardecl.array_length <= 0) {
+        ErrorList_printf(ERROR_LIST, "Type error on line %d: array variable declared with non-positive length", node->source_line);
+    }
+    return;
+}
+
+void AnalysisVisitor_screen_funcdecl (NodeVisitor* visitor, ASTNode* node)
+{
+    DecafType return_type = node->funcdecl.return_type;
+    DATA->current_function_type = return_type;
+    return;
+}
+
+void AnalysisVisitor_check_return (NodeVisitor* visitor, ASTNode* node)
+{
+    DecafType return_type = (node->funcreturn.value == NULL) ? VOID : GET_INFERRED_TYPE(node->funcreturn.value);
+    if (return_type != DATA->current_function_type) {
+        ErrorList_printf(ERROR_LIST, "Type error on line %d: return type does not match function return type", node->source_line);
+    }
+    return;
+}
+
+
+void AnalysisVisitor_infer_location (NodeVisitor* visitor, ASTNode* node)
+{
+    Symbol* symbol = lookup_symbol_with_reporting(visitor, node, node->location.name);
+    if (symbol != NULL) {
+        SET_INFERRED_TYPE(symbol->type);
+    } else {
+        SET_INFERRED_TYPE(UNKNOWN);
+    }
+    return;
+}
+
+void AnalysisVisitor_check_location (NodeVisitor* visitor, ASTNode* node)
+{
+    if(node->location.index != NULL) {
+        Symbol* symbol = lookup_symbol_with_reporting(visitor, node, node->location.name);
+        if(symbol != NULL && symbol->symbol_type != ARRAY_SYMBOL) {
+            ErrorList_printf(ERROR_LIST, "Type error on line %d: non-array variable used with index", node->source_line);
+        }
+        
+        // if(symbol != NULL && symbol->symbol_type == SCALAR_SYMBOL){
+        //     printf("SCALAR SYMBOL\n");
+        // } else if(symbol != NULL && symbol->symbol_type == ARRAY_SYMBOL){
+        //     printf("ARRAY SYMBOL\n");
+        // } else if(symbol != NULL && symbol->symbol_type == FUNCTION_SYMBOL){
+        //     printf("FUNCTION SYMBOL\n");
+        // }
+        
+        DecafType index_type = GET_INFERRED_TYPE(node->location.index);
+        if(index_type != INT) {
+            ErrorList_printf(ERROR_LIST, "Type error on line %d: array index is not an integer", node->source_line);
+        }
+    } else {
+        Symbol* symbol = lookup_symbol_with_reporting(visitor, node, node->location.name);
+        if(symbol != NULL && symbol->symbol_type != SCALAR_SYMBOL) {
+            ErrorList_printf(ERROR_LIST, "Type error on line %d: array variable used without index", node->source_line);
+        }
+        // if(symbol != NULL && symbol->symbol_type == SCALAR_SYMBOL){
+        //     printf("SCALAR SYMBOL\n");
+        // } else if(symbol != NULL && symbol->symbol_type == ARRAY_SYMBOL){
+        //     printf("ARRAY SYMBOL\n");
+        // } else if(symbol != NULL && symbol->symbol_type == FUNCTION_SYMBOL){
+        //     printf("FUNCTION SYMBOL\n");
+        // }
+        // Symbol_print(symbol, stdout);
+        // printf("\n");
+    }
+    return;
+}
+void AnalysisVisitor_infer_unaryop (NodeVisitor* visitor, ASTNode* node)
+{
+    switch(node->unaryop.operator) {
+        case NEGOP:
+            SET_INFERRED_TYPE(INT);
+            break;
+        case NOTOP:
+            SET_INFERRED_TYPE(BOOL);
+            break;
+        default:
+            ErrorList_printf(ERROR_LIST, "Internal error: unhandled unary operator on line %d", node->source_line);
+            SET_INFERRED_TYPE(UNKNOWN);
+    }
+    return;
+}
+
+void AnalysisVisitor_check_unaryop (NodeVisitor* visitor, ASTNode* node)
+{
+    DecafType child_type = GET_INFERRED_TYPE(node->unaryop.child);
+    
+    switch(node->unaryop.operator) {
+        case NEGOP:
+            if(child_type != INT) {
+                ErrorList_printf(ERROR_LIST, "Type error on line %d: unary - applied to non-integer operand", node->source_line);
+            }
+            break;
+        case NOTOP:
+            if(child_type != BOOL) {
+                ErrorList_printf(ERROR_LIST, "Type error on line %d: unary ! applied to non-boolean operand", node->source_line);
+            }
+            break;
+        default:
+            ErrorList_printf(ERROR_LIST, "Internal error: unhandled unary operator on line %d", node->source_line);
+    }
+    return;
 }
 
 void AnalysisVisitor_infer_binaryop (NodeVisitor* visitor, ASTNode* node)
@@ -171,6 +286,10 @@ NodeVisitor* ListVariablesVisitor_new () {
 
 ErrorList* analyze (ASTNode* tree)
 {
+    if(tree == NULL) {
+        ErrorList* errors = ErrorList_new();
+        return errors;
+    }
     /* allocate analysis structures */
     NodeVisitor* v = NodeVisitor_new();
     v->data = (void*)AnalysisData_new();
@@ -178,6 +297,13 @@ ErrorList* analyze (ASTNode* tree)
 
     /* BOILERPLATE: TODO: register analysis callbacks */
     v->previsit_literal = AnalysisVisitor_infer_literal;
+    v->previsit_vardecl = AnalysisVisitor_screen_vardecl;
+    v->previsit_funcdecl = AnalysisVisitor_screen_funcdecl;
+    v->postvisit_return = AnalysisVisitor_check_return;
+    v->previsit_location = AnalysisVisitor_infer_location;
+    v->postvisit_location = AnalysisVisitor_check_location;
+    v->previsit_unaryop = AnalysisVisitor_infer_unaryop;
+    v->postvisit_unaryop = AnalysisVisitor_check_unaryop;
     v->previsit_binaryop = AnalysisVisitor_infer_binaryop;
     v->postvisit_binaryop = AnalysisVisitor_check_binaryop;
     /* perform analysis, save error list, clean up, and return errors */
