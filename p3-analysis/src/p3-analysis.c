a049c1b109f12e7c6d9af5f0b4c90c84f746b8f9
pure_jerome <donfacjc@dukes.jmu.edu>
2025-10-17 16:18:41 -0400

    fixed small function as vairable error

--- p3-analysis/src/p3-analysis.c
+++ p3-analysis/src/p3-analysis.c
@@ -482,9 +482,17 @@ AnalysisVisitor_check_location (NodeVisitor *visitor, ASTNode *node)
     {
       if (symbol != NULL && symbol->symbol_type != SCALAR_SYMBOL)
         {
-          ErrorList_printf (ERROR_LIST,
+          if(symbol->symbol_type == ARRAY_SYMBOL)
+            {
+              ErrorList_printf (ERROR_LIST,
                             "Array '%s' accessed without index on line %d",
                             node->location.name, node->source_line);
+            }
+          else {
+            ErrorList_printf (ERROR_LIST,
+                            "Function '%s' accessed as a variable on line %d",
+                            node->location.name, node->source_line);
+          }
         }
     }
   return;
